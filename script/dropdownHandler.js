dee242cd3de994882b605e73caab2e3353af5a5b
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-02-16 16:03:29 +0000

    Automatically save last state of dropdown

--- script/dropdownHandler.js
+++ script/dropdownHandler.js
@@ -117,10 +117,49 @@ function quoteModal(quotes, title) {
     });
 }
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function saveCurrentOption(value) {
+    // Make a cookie
+    setCookie("currentOption", value, 1);
+}
+
+function setCurrentOption() {
+    var currentOption = getCookie("currentOption");
+    if (currentOption != null) {
+        var selections = document.getElementsByClassName("selection")[0];
+        selections.value = currentOption;
+        
+        // Trigger change event
+        var event = new Event('change');
+        selections.dispatchEvent(event);
+    }
+}
+
 selections.addEventListener("change", (event) => {
     const selectedOption = event.target.value;
 
     // selectionData.innerHTML = "";
+
+    saveCurrentOption(selectedOption);
 
     if (selectedOption === "default") {
         selectionData.style.minHeight = `0px`;
@@ -360,3 +399,5 @@ selectionData.addEventListener("click", (event) => {
         quoteModal(JSON.parse(decodeURIComponent(atob(Premade.Lear[act][scene]))), `${topic} | ${act} ${scene} Quotes`);
     }
 });
+
+setCurrentOption();
