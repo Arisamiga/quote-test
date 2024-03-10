6f850115175169af410de36c7931f56e0a14cb8c
Aris <64918822+Arisamiga@users.noreply.github.com>
2024-03-10 12:56:05 +0000

    Ability for timer modal + addition to options

--- script/optionHandle.js
+++ script/optionHandle.js
@@ -1,5 +1,121 @@
 (function () {
 
+function createPopup(buttons, type, content) {
+    return new Promise((resolve) => {
+        const popup = document.getElementById("popup")
+        const button1 = document.getElementById("button1")
+        const button2 = document.getElementById("button2")
+        const button3 = document.getElementById("button3")
+        const title = document.getElementById("popupTitle")
+        const text = document.getElementById("popupText")
+        const popupContent = document.getElementById("popupContent")
+        popup.style.overflow = "auto"
+        document.body.style.overflow = "hidden"
+        if (buttons === "yn") {
+            button1.style.display = "block"
+            button2.style.display = "block"
+            button1.innerHTML = "Yes"
+            button2.innerHTML = "No"
+            button1.addEventListener("click", () => {
+                resolve(true);
+                closePopup()
+            });
+            button2.addEventListener("click", () => {
+                resolve(false);
+                closePopup()
+            });
+        }
+        else if (buttons === "ok") {
+            button1.style.display = "block"
+            button1.innerHTML = "OK"
+            button1.addEventListener("click", () => {
+                resolve(true);
+                closePopup()
+            })
+        }
+        else if (buttons === "blank") {
+        }
+        else if (buttons === "force") {
+            button1.style.display = "block";
+            button2.style.display = "block";
+            button1.innerHTML = "OK";
+            button2.innerHTML = "Complete Anyway";
+            button1.addEventListener("click", () => {
+                resolve(false)
+                closePopup();
+            })
+            button2.addEventListener("click", () => {
+                resolve(true);
+                closePopup();
+            })
+        }
+        else if (buttons === "timerSettings") {
+            button1.style.display = "block";
+            button1.innerHTML = "OK";
+
+            button1.addEventListener("click", () => {
+                resolve(true);
+                closePopup();
+            })
+        }
+        else {
+            button1.style.display = "block"
+            button1.innerHTML = "debug: Invalid Button Type!!"
+        }
+
+        if (type === "info") {
+            title.innerHTML = "ℹ️ Information"
+            title.style.color = "#67f5ff"
+        }
+        else if (type === "caution") {
+            title.innerHTML = "⚠️Caution";
+            title.style.color = "#ffdf77";
+        }
+        else if (type === "warning") {
+            title.innerHTML = "❗ Warning";
+            title.style.color = "#cd1000"
+        }
+        else if (type === "error") {
+            title.innerHTML = "❗ Error";
+            title.style.color = "#cd1000";
+        }
+        else if (type === "timer") {
+            title.innerHTML = "⏲️ Timer";
+        }
+        else {
+            title.innerHTML = type;
+        }
+        text.innerHTML = content;
+        popup.style.display = "block"
+        window.addEventListener("click", (event) => {
+            if (event.target == popup) {
+                closePopup();
+            }
+        }
+        )
+        const closeBtn = document.getElementById("closePopup")
+        function closePopup() {
+            popup.style.overflow = "hidden";
+            popupContent.style.animation = "contentOut 0.6s";
+            popup.style.animation = "bgOut 0.6s";
+            popup.addEventListener("animationend", (event) => {
+                if (event.animationName === "bgOut") {
+                    popup.style.display = "none"
+                    button1.style.display = "none";
+                    button2.style.display = "none";
+                    button3.style.display = "none";
+                    popup.style.animation = "";
+                    popupContent.style.animation = "";
+                    document.body.style.overflow = "auto";
+                }
+            })
+        }
+        closeBtn.addEventListener("click", () => {
+            closePopup()
+        })
+    });
+}
+
 var intensity = document.getElementById("intensityRange")
 var intensityOutput = document.getElementById("intensityValue")
 intensityOutput.innerHTML = intensity.value;
@@ -49,10 +165,85 @@ optionText.addEventListener("click", function (element) {
         });
     }
 });
-})();
 
 const timerBtn1 = document.getElementById("timerBtn1")
 
 timerBtn1.addEventListener("click", () => {
-    createPopup("blank", "info", "The timer feature has not been implemented yet!")
+    createPopup("timerSettings", "timer", "Select your time (In seconds): <br>")
+
+    var input = document.createElement("input");
+    input.type = "number";
+    input.id = "timerInput";
+    input.placeholder = "Enter time in seconds";
+    input.min = "0"; // restrict input to positive numbers
+    input.classList.add("timerInput");
+
+    document.getElementById("popupText").appendChild(input);
+
+    // var br = document.createElement("br");
+    // document.getElementById("popupText").appendChild(br);
+
+    var buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+    document.getElementById("popupText").appendChild(buttonContainer);
+
+    // Add +1 -1 buttons
+    var plus1 = document.createElement("button");
+    plus1.innerHTML = "+1";
+    plus1.classList.add("plus1");
+    buttonContainer.appendChild(plus1);
+
+    var minus1 = document.createElement("button");
+    minus1.innerHTML = "-1";
+    minus1.classList.add("minus1");
+    buttonContainer.appendChild(minus1);
+
+    // Add event listeners to the buttons
+    plus1.addEventListener("click", () => {
+        var num = parseInt(input.value)
+
+        if (isNaN(num) || num < 0) {
+            input.value = 0;
+            num = 0;
+        }
+
+        input.value = num + 1;
+    });
+
+    minus1.addEventListener("click", () => {
+        var num = parseInt(input.value)
+
+        if (isNaN(num) || num < 1) {
+            input.value = 1;
+            num = 1;
+        }
+
+        input.value = num - 1;
+    });
+
+    // Wait till the user clicks OK
+    var timerInput = document.getElementById("timerInput");
+    var button1 = document.getElementById("button1");
+    button1.addEventListener("click", () => {
+        var num = parseInt(timerInput.value);
+
+        const optionField = document.getElementsByClassName("optionsField")[0];
+
+        if (document.getElementsByClassName("timerText").length > 0) {
+            optionField.removeChild(document.getElementsByClassName("timerText")[0]);
+        }
+
+        const timerText = document.createElement("div")
+        timerText.classList.add("timerText");
+        timerText.innerHTML = `Timer: ${num} seconds`;
+        optionField.appendChild(timerText);
+
+        let newOptions = JSON.parse(localStorage.getItem("options")) ?? {};
+        newOptions.timer = num;
+
+        localStorage.setItem("options", JSON.stringify(newOptions));
+    });
+
 })
+
+})();
